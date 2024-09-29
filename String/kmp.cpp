da22a2fc750f7835b15a2f2cd475f65784572bc0
Sarwar82 <105103043+Sarwar82@users.noreply.github.com>
2024-09-29 13:02:54 +0600

    Create kmp.cpp

---
+++ String/kmp.cpp
@@ -0,0 +1,29 @@
+vector<int>lps;
+string s;
+vector<int>f(string &s)
+{
+    int n = s.size();
+    vector<int>lps(n);
+    for(int i = 1; i < n; i ++){
+        int j = lps[i - 1];
+        while(j >= 0 && s[i] != s[j]){
+            if(j == 0) j = -1;
+            else j = lps[j - 1];
+        }
+ 
+        lps[i] = ++j;
+    }
+ 
+    return lps;
+}
+
+// matched upto ith charecter...transition if next charecter is ch
+int f(int i, char ch)
+{
+    
+    while(i >= 0 && s[i] != ch){
+        if(i == 0) i = -1;
+        else i = lps[i - 1];
+    }
+    return i + 1;
+}
